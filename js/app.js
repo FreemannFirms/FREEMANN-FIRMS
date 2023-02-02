f38badee16fdb8e11e35cefe394bac903a42ec54
freemann firms <freemannfirm@gmail.com>
2023-02-02 21:18:08 +0100

    Made new changes

--- js/app.js
+++ js/app.js
@@ -9,4 +9,34 @@ buttonEl.addEventListener('click', function(){
     }
 })
 
+function setupTabs(){
+    document.querySelectorAll(".tabs-button").forEach(button => {
+        button.addEventListener("click", () => {
+            button.parentElement.childNodes.forEach(btn => {
+                if(btn?.classList?.contains('tabs-button')){
+                    btn.classList.remove('active')
+                }
+            })
+
+            button.classList.add('active')
+
+            button.parentElement.parentElement.childNodes.forEach(content => {
+                if('#' + content?.id === button?.dataset?.target){
+                    content.classList.add('tabs-content-active')
+                }
+                else{
+                    content?.classList?.remove('tabs-content-active')
+                }
+            })
+        });
+    });
+}
+
+document.addEventListener("DOMContentLoaded", () =>{
+    setupTabs();
+
+    document.querySelectorAll(".tabs").forEach(tabsContainer =>{
+        tabsContainer.querySelector(".tabs-sidebar .tabs-button").click();
+    });
+});
 
